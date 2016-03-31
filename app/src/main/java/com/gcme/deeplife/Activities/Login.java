3cecc109fe3ae19b4c82d9b73060590b835191cd
BENGEOS <george.beng@gmail.com>
2016-03-31 19:10:57 +0300

    integrate login with the back end API

--- app/src/main/java/com/gcme/deeplife/Activities/Login.java
+++ app/src/main/java/com/gcme/deeplife/Activities/Login.java
@@ -10,14 +10,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.gcme.deeplife.MainActivity;
 import com.gcme.deeplife.R;
+import com.github.kittinunf.fuel.Fuel;
+import com.github.kittinunf.fuel.core.FuelError;
+import com.github.kittinunf.fuel.core.Handler;
+import com.github.kittinunf.fuel.core.Request;
+import com.github.kittinunf.fuel.core.Response;
+
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kotlin.Pair;
 
 
 public class Login extends AppCompatActivity{
@@ -56,8 +71,6 @@ public class Login extends AppCompatActivity{
 		ed_phoneNumber.addTextChangedListener(new MyTextWatcher(ed_phoneNumber));
 		ed_password.addTextChangedListener(new MyTextWatcher(ed_phoneNumber));
 
-
-
 		setButtonActions();
 	}
 
@@ -68,14 +81,13 @@ public class Login extends AppCompatActivity{
 		*/
 
 		bt_login.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
+            @Override
+            public void onClick(View v) {
                 submitForm();
-
             }
 
 
-		});
+        });
 
 		/*
 				Button login works
@@ -106,10 +118,37 @@ public class Login extends AppCompatActivity{
         if (!validatePassword()) {
             return;
         }
-        Intent register = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(register);    }
+        List<Pair<String, String>> Send_Param;
+        Send_Param = new ArrayList<Pair<String, String>>();
+        Send_Param.add(new kotlin.Pair<String, String>("User_Name", ed_phoneNumber.getText().toString()));
+        Send_Param.add(new kotlin.Pair<String, String>("User_Pass", ed_password.getText().toString()));
+        Send_Param.add(new kotlin.Pair<String, String>("Service", "Log_In"));
+        Send_Param.add(new kotlin.Pair<String, String>("Param", "[]"));
+        Toast.makeText(this,"Loging Started",Toast.LENGTH_LONG).show();
+        Fuel.post("http://192.168.0.32/SyncSMS/public/deep_api", Send_Param).responseString(new Handler<String>() {
+            @Override
+            public void success(Request request, Response response, String s) {
+                try {
+                    JSONObject myObject = (JSONObject) new JSONTokener(s).nextValue();
+                    Log.i("Log_In", "Server Response -> \n" + myObject.toString());
+                    if (!myObject.isNull("Response")) {
+                        Intent register = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(register);
+                    }else{
+                        Toast.makeText(Login.this, "Invalid User Account used", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (Exception e) {
+                    Toast.makeText(Login.this,"Error Occured\n"+e.toString(),Toast.LENGTH_LONG).show();
+                }
 
+            }
 
+            @Override
+            public void failure(Request request, Response response, FuelError fuelError) {
+                Toast.makeText(Login.this, "Invalid User Account used", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
     private boolean validatePhone() {
         if (ed_phoneNumber.getText().toString().trim().isEmpty()) {
             inputLayoutPhone.setError(getString(R.string.err_msg_phone));
